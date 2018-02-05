402f98689b81301ce4b1a1624650eda29e0c679d
Dhiren Patel <dhiren@jkbsoftware.com>
2018-02-05 15:30:33 +0530

    Add project files.

---
+++ CustomIdentityMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomIdentityMVC.MvcApplication" Language="C#" %>
---
+++ CustomIdentityMVC/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomIdentityMVC.Startup))]
+
+namespace CustomIdentityMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
